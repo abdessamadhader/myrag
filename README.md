ea3549b0bdda7b851d432f86bf16eac583f4e688
abdessamadhader <136916776+abdessamadhader@users.noreply.github.com>
2024-04-18 17:08:25 -0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# RAG Retriever Application with Streamlit
+This application leverages the Retrieval-Augmented Generation (RAG) technique to answer questions based on the content extracted from PDF files or web pages. Built with Streamlit, PyPDF2, and the LangChain community libraries, this application provides a user-friendly interface to upload PDF documents or input web links, and ask questions based on the provided documents.
+
+![WhatsApp Image 2024-04-15 at 23 02 53_5224fefd](https://github.com/abdessamadhader/myrag/assets/136916776/18534f14-9b79-4185-8cbc-81ea8d9dea0a)
---
+++ rag.py
@@ -0,0 +1,121 @@
+import streamlit as st
+import os 
+import PyPDF2
+# install dependencies
+# !pip install langchain-community
+
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.vectorstores import Chroma
+from langchain_community import embeddings
+from langchain_community.chat_models import ChatOllama
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_nomic.embeddings import NomicEmbeddings
+
+from langchain.schema import Document
+
+
+# nk-soZ88R5mcboBQP5r0TmJLyWHiRuwoKUrQ8CKIoh7__0
+
+def read_pdf(file):
+    pdfReader = PyPDF2.PdfReader(file)
+    all_page_text = ""
+    for page in pdfReader.pages:
+        all_page_text += page.extract_text() + "\n"
+    return all_page_text
+
+
+
+
+def retriever(doc, question):
+    model_local = ChatOllama(model="mistral")
+    doc = Document(page_content=doc)
+    doc = [doc]
+    text_splitter = CharacterTextSplitter.from_tiktoken_encoder(chunk_size=800, chunk_overlap=0)
+    doc_splits = text_splitter.split_documents(doc)
+
+    vectorstore = Chroma.from_documents(
+        documents=doc_splits,
+        collection_name="rag-chroma",
+        embedding=NomicEmbeddings(model="nomic-embed-text-v1.5"),
+    )
+    retriever = vectorstore.as_retriever(k=2)
+    after_rag_template = """Answer the question based only on the following context:
+    {context}
+    Question: {question}
+    if there is no answer, please answer with "I m sorry, the context is not enough to answer the question."
+    """
+    after_rag_prompt = ChatPromptTemplate.from_template(after_rag_template)
+    after_rag_chain = (
+        {"context": retriever, "question": RunnablePassthrough()}
+        | after_rag_prompt
+        | model_local
+        | StrOutputParser()
+    )
+
+    return after_rag_chain.invoke(question)
+
+# now we will add a rag but with links to the retriever
+
+
+
+# retrieve the website content
+
+
+
+def retriever_with_links(question, links):
+    model_local = ChatOllama(model="mistral")
+    docs = [WebBaseLoader(url).load() for url in links]
+    docs_list = [item for sublist in docs for item in sublist]
+    text_splitter = CharacterTextSplitter.from_tiktoken_encoder(chunk_size=7500, chunk_overlap=100)
+    doc_splits = text_splitter.split_documents(docs_list)
+
+    vectorstore = Chroma.from_documents(
+        documents=doc_splits,
+        collection_name="rag-chroma",
+        embedding=NomicEmbeddings(model="nomic-embed-text-v1.5"),
+    )
+    retriever = vectorstore.as_retriever(k=2)
+    after_rag_template = """Answer the question based only on the following context:
+    {context}
+    Question: {question}
+    """
+    after_rag_prompt = ChatPromptTemplate.from_template(after_rag_template)
+    after_rag_chain = (
+        {"context": retriever, "question": RunnablePassthrough()}
+        | after_rag_prompt
+        | model_local
+        | StrOutputParser()
+    )
+    print("done")
+    return after_rag_chain.invoke(question)
+
+
+# lets save the history of the conversation
+
+
+# example usage streamlit with a button to switch between the two
+st.title("RAG with retriever")
+
+# switch button to choose between pdf and website
+switch = st.radio("Choose between pdf or website", ["pdf", "website"])
+if switch == "pdf":
+    file = st.file_uploader("Upload a pdf file", type=["pdf"])
+    if file:
+        st.write("File uploaded")
+        text = read_pdf(file)
+        question = st.text_input("Ask a question")
+        if st.button("Ask"):
+            answer = retriever(text, question)
+            st.write(answer)
+else:
+    links = st.text_input("Enter the links separated by commas")
+    links = links.split(",")
+    question = st.text_input("Ask a question")
+    if st.button("Ask"):
+        answer = retriever_with_links(question, links)
+        st.write(answer)
+
+
